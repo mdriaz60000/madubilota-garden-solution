64b531bf0ce32efc5430483b16686d339d84073e
mdriaz60000 <mohammadriaz60000@gmail.com>
2024-09-20 22:53:51 +0600

    added banner

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  
+        images: {
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'static.wixstatic.com',
+              port: '',
+              pathname: '/media/**',
+            },
+          ],
+        },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+'use client'
+import Banner from '@/components/Shared/Banner';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
+    <Banner></Banner>
+
 
     </div>
   );
